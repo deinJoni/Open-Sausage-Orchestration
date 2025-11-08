e58701bc8fcacaf562e1d0266b8c9a92a5723e2b
grmkris <kristjan.grm1@gmail.com>
2025-11-08 08:32:25 -0300

    qq

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -8,11 +8,9 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: z.string().min(1),
-    NEXT_PUBLIC_MERCHANT_URL: z.string().url().optional(),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
-    NEXT_PUBLIC_MERCHANT_URL: process.env.NEXT_PUBLIC_MERCHANT_URL,
   },
 });
